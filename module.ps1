c1d88ba594e5562715e42ad5a0e5919326386c23
unknown <mdphuc1110@gmail.com>
2023-11-04 18:05:29 -0400

    add functionalities

---
+++ module.ps1
@@ -0,0 +1,10 @@
+$location = $(Invoke-Command {Get-Location}).ToString()
+$drive = $($location)[0..1] -join ''
+$user = $env:USERPROFILE
+$username = $env:USERNAME
+
+mv ./module/remotenv.psm1 .
+mv ./remotenv.ps1 ./module
+cd "C:\Windows\System32\WindowsPowerShell\v1.0\Modules"
+mkdir remotenv
+cp -R ./* "C:\Windows\System32\WindowsPowerShell\v1.0\Modules\remotenv"
--- test.ps1
+++
@@ -1,4 +0,0 @@
-# $Content = Get-Content config.txt
-# Write-Host $Content
-
-Invoke-Command {docker exec -it c898a564726f sh -c "ping -c 1 -W 1 172.17.0.3; echo $?"}
