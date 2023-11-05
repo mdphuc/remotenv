471e81bc96814023a18d7e9181d7d85c762d40e5
unknown <mdphuc1110@gmail.com>
2023-11-04 23:06:06 -0400

    fix silly module bug

--- module/remotenv.psm1
+++ module/remotenv.psm1
@@ -11,50 +11,60 @@ Function remotenv{
 
   if($option -ne "build" -And $option -ne "cmdlets"){
     Write-Host "
-.\remotenv.ps1
-  build:            set up remote dev environment
-                        docker: build docker container
-                        remote machine: use remote machine as remote dev environment
-  cmdlets:          build powershell cmdlets
+  remotenv
+    build:            set up remote dev environment
+                          docker: build docker container
+                          remote machine: use remote machine as remote dev environment
+    cmdlets:          build powershell cmdlets
     "
   }elseif($option -eq "build"){
     $project = $(Invoke-Command {Split-Path -Path (Get-Location) -Leaf}).ToString()
     cd "$($env:USERPROFILE)\AppData\Local\Programs\Python\Python$($python_version)"
-    Invoke-Command {.\python.exe "$($drive)/Windows/System32/WindowsPowerShell/v1.0/Modules/remotenv/readyaml.py" "$($location)"}
+    Invoke-Command {.\python.exe "$($drive)/Windows/System32/WindowsPowerShell/v1.0/Modules/readyaml.py" "$($location)"}
 
     $setting = Read-Host "Mode (docker, remote machine)"
-    $Content = Get-Content "$($drive)/Windows/System32/WindowsPowerShell/v1.0/Modules/remotenv/config.txt"
+    $Content = Get-Content "$($location)/config.txt"
+    Invoke-Command {rm "$($location)/config.txt"}
 
     if($setting -ne "docker" -And $setting -ne "remote machine"){
       Write-Host "Invalid mode"
     }elseif($setting -eq "docker"){
-      try{
-        $environment = $Content[0].Split(":")[1]
-        $framework = $Content[1].Split(":")[1]
-        cd $location
-        Invoke-Command {docker run --name $($project) -v .:/$($project) --workdir /$($project) -itd ubuntu}
-        $docker_name = Invoke-Command {docker container ls --all --quiet --filter "name=$($project)"}
-        Invoke-Command {docker exec -it $docker_name sh -c "apt update"}
-        Invoke-Command {docker exec -it $docker_name sh -c "apt upgrade -y"}
-        if($environment -eq "powershell"){
-          Invoke-Command {docker exec -it $docker_name sh -c "apt install -y wget apt-transport-https software-properties-common"}
-          Invoke-Command {docker exec -it $docker_name sh -c "apt install wget"}
-          Invoke-Command {docker exec -it $docker_name sh -c "wget -q 'https://packages.microsoft.com/config/ubuntu/22.04/packages-microsoft-prod.deb'"}
-          Invoke-Command {docker exec -it $docker_name sh -c "dpkg -i packages-microsoft-prod.deb"}
-          Invoke-Command {docker exec -it $docker_name sh -c "rm packages-microsoft-prod.deb"}
+      $mode = Read-Host "Mode (build, reset)"
+      if($mode -ne "build" -And $mode -ne "reset"){
+        Write-Host "Invalid Option"
+      }elseif($option -eq "build"){
+        try{
+          $environment = $Content[0].Split(":")[1]
+          $framework = $Content[1].Split(":")[1]
+          cd $location
+          Invoke-Command {docker run --name $($project) -v .:/$($project) --workdir /$($project) -itd ubuntu}
+          $docker_name = Invoke-Command {docker container ls --all --quiet --filter "name=$($project)"}
           Invoke-Command {docker exec -it $docker_name sh -c "apt update"}
-          Invoke-Command {docker exec -it $docker_name sh -c "apt install -y powershell"}
+          Invoke-Command {docker exec -it $docker_name sh -c "apt upgrade -y"}
+          if($environment -eq "powershell"){
+            Invoke-Command {docker exec -it $docker_name sh -c "apt install -y wget apt-transport-https software-properties-common"}
+            Invoke-Command {docker exec -it $docker_name sh -c "apt install wget"}
+            Invoke-Command {docker exec -it $docker_name sh -c "wget -q 'https://packages.microsoft.com/config/ubuntu/22.04/packages-microsoft-prod.deb'"}
+            Invoke-Command {docker exec -it $docker_name sh -c "dpkg -i packages-microsoft-prod.deb"}
+            Invoke-Command {docker exec -it $docker_name sh -c "rm packages-microsoft-prod.deb"}
+            Invoke-Command {docker exec -it $docker_name sh -c "apt update"}
+            Invoke-Command {docker exec -it $docker_name sh -c "apt install -y powershell"}
 
+          }
+          if($framework -eq "python" -Or $framework -eq "python3" -Or $framework -eq "python2"){
+            Invoke-Command {docker exec -it $docker_name sh -c "apt install -y python3"}
+          }else{
+            Invoke-Command {docker exec -it $docker_name sh -c "apt install -y $($framework)"}
+          }
+          Write-Host "$($docker_name)"
+          Invoke-Command {docker exec -it $docker_name /bin/bash}
+        }catch{
+            Write-Host $_
         }
-        if($framework -eq "python" -Or $framework -eq "python3" -Or $framework -eq "python2"){
-          Invoke-Command {docker exec -it $docker_name sh -c "apt install -y python3"}
-        }else{
-          Invoke-Command {docker exec -it $docker_name sh -c "apt install -y $($framework)"}
-        }
-        Write-Host "$($docker_name)"
-        Invoke-Command {docker exec -it $docker_name /bin/bash}
-      }catch{
-          Write-Host $_
+      }elseif($mode -eq "reset"){
+        $docker_name = Invoke-Command {docker container ls --all --quiet --filter "name=$($project)"}
+        Invoke-Command {docker stop $($docker_name)}
+        Invoke-Command {docker system prune}
       }
     }else{
       $mode = Read-Host "Mode (build, reset)"
@@ -72,13 +82,14 @@ Function remotenv{
           Invoke-Command {docker exec -it $docker_name sh -c "apt update"}
           Invoke-Command {docker exec -it $docker_name sh -c "apt upgrade -y"}
           Invoke-Command {docker exec -it $docker_name sh -c "apt install openssh-client -y"}
+          Invoke-Command {docker exec -it $docker_name sh -c "apt install systemctl -y"}
           Invoke-Command {docker exec -it $docker_name sh -c "ssh-keygen -t rsa"}
           Invoke-Command {docker exec -it $docker_name sh -c "mkdir /$($project)/.ssh"}
           Invoke-Command {docker exec -it $docker_name sh -c "cp /root/.ssh/id_rsa.pub /$($project)/.ssh"}
           Invoke-Command {docker exec -it $docker_name sh -c "ssh $($username)@$($ip) -i /root/.ssh/id_rsa 'mkdir /$($project)'"}
           Invoke-Command {docker exec -it $docker_name sh -c "echo 'while true; do scp -i /root/.ssh/id_rsa -r /$($project) $($username)@$($ip):/$($project) 2>&1 </dev/null; sleep 30; done' > /$($project)/bash/monitor.sh"}
           Invoke-Command {docker exec -it $docker_name sh -c "chmod +x /$($project)/bash/monitor.sh"}
-          Invoke-Command {docker exec -it $docker_name sh -c "cp /$($project)/bash/monitor.service /lib/systemd/system/monitor.sservice"}
+          Invoke-Command {docker exec -it $docker_name sh -c "cp /$($project)/bash/monitor.service /lib/systemd/system/monitor.service"}
           Invoke-Command {docker exec -it $docker_name sh -c "cp /$($project)/bash/monitor.sh /usr/bin/monitor.sh"}
           Invoke-Command {docker exec -it $docker_name sh -c "systemctl daemon-reload"}
           Invoke-Command {docker exec -it $docker_name sh -c "systemctl enable monitor.service"}
@@ -90,7 +101,7 @@ Function remotenv{
         Invoke-Command {docker exec -it $docker_name sh -c "ssh $($username)@$($ip) -i /root/.ssh/id_rsa 'rm -rf /$($project)'"}
       }
     }
-  }elseif($option -eq "cmdlets"){
+  }else{
     Invoke-Command {powershell -f ./module.ps1}
   }
 
